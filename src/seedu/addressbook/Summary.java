87451e426031ceedf301bf2d3dc7a4efd1c5b9cb
KhawYewOnn <khaw.yewonn@gmail.com>
2017-09-14 00:14:43 +0800

    Add Summary.java via add-summary branch. This java file has no function.

---
+++ src/seedu/addressbook/Summary.java
@@ -0,0 +1,8 @@
+package seedu.addressbook;
+
+/**
+ * This class is created to do nothing. I am using to learn more about
+ * git pull request and the mistake newbie do when creating pull request.
+ */
+public class Summary {
+}
